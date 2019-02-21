3c754926432bcc5d29b53c8dbbea40c474cae301
Unknown <connor.mcnutt.2022@student.lhschools.org>
2019-02-21 14:07:07 -0500

    Added two files and added four Methods

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -3,6 +3,24 @@ package com.LickingHeights;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        printHelloWorld();
+        printHello("Universe");
+        multiply(2,4);
+        System.out.println(divideDouble(5.25,3.09));
+    }
+
+    public static void printHelloWorld(){
+        System.out.println("Hello World");
+    }
+    public static void printHello(String noun){
+        System.out.println("Hello "+noun);
+    }
+    public static void multiply(int firstNumber, int secondNumber){
+        System.out.println(firstNumber*secondNumber);
+    }
+    public static double divideDouble(double firstNumber, double secondNumber){
+        double answer = firstNumber/secondNumber;
+        return answer;
     }
 }
